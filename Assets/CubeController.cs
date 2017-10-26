a325355cd9eccd6c8b7607edfc0ae815163ba172
SyotaroYamashita <nkc.syotaro.yamashita@gmail.com>
2017-10-26 10:59:18 +0900

    弾を投げる機能を追加 投げるたびに倍になる不具合あり

---
+++ Assets/CubeController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+    public void Shoot(Vector3 dir) {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+}
---
+++ Assets/CubeGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeGenerator : MonoBehaviour {
+
+    public GameObject spherePrefab;
+    void Start () {
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject cube =
+                Instantiate(spherePrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            cube.GetComponent<CubeController>().Shoot(
+                worldDir.normalized * 2000);
+        }
+	}
+}
